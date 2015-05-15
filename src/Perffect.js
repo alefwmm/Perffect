86881456f58891591cc51e0c18e0e166ece00b22
Alef Miranda <alefwmm@gmail.com>
2015-05-15 15:50:32 -0300

    Refactor. Production ready.

--- src/Perffect.js
+++ src/Perffect.js
@@ -15,34 +15,62 @@ if(!window.Perffect) {
         /*
         * Constructor
         *
-        * config {
-        *     container: the node to be managed
-        *     selector: the children selector
-        *     gutter: size in pixels of the relative gutter
-        *     notAuto: auto starts Perffect if false
+        * config: {
+        *     container: [Object]   [required] the container node
+        *     selector:  [String]   [optional] the selector of the elements to be managed
+        *     select:    [Function] [optional] if selector not set, this function is used
+        *     gutter:    [Number]   [optional] size in pixels of the relative gutter
+        *     auto:      [Boolean]  [optional] auto starter
         * }
         */
         function LayoutManager(config) {
+            // REQUIRED
             this.container = config.container;
-            this.selector = config.selector;
-            this.gutter = config.gutter || 0;
-            this.notAuto = config.notAuto || false;
 
+            // OPTIONAL
+            this.select = config.select  || childrenSelector(config.container, config.selector) || null;
+            this.gutter = config.gutter  || 0;
+            this.auto   = config.notAuto || true;
+
+            // INTERNAL
             this.elements = null;
 
-            this.$arrangePointer = this.arrange.bind(this);
+            // SPECIAL
+            this.$rearrangePointer = this.rearrange.bind(this);
             this.$lastRAF = null;
-            this.started = false;
 
-            if (!this.notAuto) this.rearrange();
+            if (this.auto) this.rearrange();
         }
 
-        // Fixed timeout of 33 miliseconds (30 FPS)
+        /*
+        * Returns a function to select the required elements
+        *
+        * container: [Object] [required] container object
+        * selector:  [String] [required] class selector
+        *
+        */
+        function childrenSelector(container, selector) {
+            return function () {
+                return container.querySelectorAll(selector);
+            };
+        }
+
+        /*
+        * Binds setTimeout to 33ms
+        *
+        * callback: [Function] [required] The callback function
+        */
         function getTimeout(callback) {
             return window.setTimeout(callback, 33);
         }
 
-        // Select the correct Request Animation Frame function
+        /*
+        * Cross-browser Request Animation Frame
+        *
+        * callback: [Function] [required] The callback function
+        * thisArg:  [Object]   [optional] Callback reset of the this pointer
+        * args:     [Array]    [optional] Callback arguments
+        */
         function requestFrame(callback, thisArg, args) {
             var raf =
                 window.requestAnimationFrame ||
@@ -55,7 +83,11 @@ if(!window.Perffect) {
             });
         }
 
-        // Select the corrent Cancel Animation Frame function
+        /*
+        * Cross-browser Cancel Animation Frame
+        *
+        * id: [?] [required] Returned ID from Request Animation Frame
+        * */
         function cancelFrame(id) {
             var caf =
                 window.cancelAnimationFrame ||
@@ -66,115 +98,75 @@ if(!window.Perffect) {
             return caf(id);
         }
 
-        // Rearranges the whole layout
-        LayoutManager.prototype.rearrange = function () {
-
-            if (!this.started) {
-                Resizer.add(this.container, function () {
-                    if (this.$lastRAF) cancelFrame(this.$lastRAF);
-                    this.$lastRAF = requestFrame(this.rearrange, this);
-                }.bind(this));
-                this.started = true;
-            }
-
-            this.refresh();
-            this.attachEvents();
-            this.layout();
-        }
-
         /*
-        * Resize event attachment
+        * Calculates container height and children positions
         *
-        * Attachs a resize event to every container children
+        * container: [Object]   [required] The container
+        * elements:  [NodeList] [required] The elements to be managed
+        * gutter:    [Number]   [required] Distance between the elements
         */
-        LayoutManager.prototype.attachEvents = function () {
-            for(
-                var i = 0,
-                    length = this.elements.length,
-                    node = this.elements.item(i);
-                i < length;
-                i++,
-                    node = this.elements.item(i)
-            ) {
-                Resizer.add(node, function () {
-                    if (this.$lastRAF) cancelFrame(this.$lastRAF);
-                    this.$lastRAF = requestFrame(this.$arrangePointer, this);
-                }.bind(this));
-            }
-        }
+        function layout(container, elements, gutter) {
+            var width, offset, columnCount, columnWidth, columnOffset;
 
-        /*
-        * Calls refresh and layout functions
-        */
-        LayoutManager.prototype.arrange = function () {
-            this.refresh();
-            this.layout();
-        }
+            columnWidth = elements.item(0).offsetWidth + gutter;
+            width       = container.offsetWidth;
 
-        /*
-        * Registers the children elements of the container
-        */
-        LayoutManager.prototype.refresh = function () {
-            if (typeof this.selector === "string") {
-                this.elements = this.container.querySelectorAll(this.selector);
-            } else {
-                this.elements = this.selector();
-            }
+            columnCount  = Math.trunc(width / columnWidth);
+            columnCount  = (columnCount > elements.length) ? elements.length : columnCount;
+            offset       = (width - columnCount * columnWidth - gutter) / 2;
+            columnOffset = new Array(columnCount);
 
-            this.container.style.position = "relative";
-        }
+            for (var i = 0; i < columnCount; i++) columnOffset[i] = 0;
 
-        /*
-        * Calculates container size and children positions
-        */
-        LayoutManager.prototype.layout = function () {
-            var containerWidth, containerHeight, elementWidth, columns, count, multiplier, leftSpace;
-
-            if(!this.elements.length) return;
-
-            elementWidth = this.elements.item(0).offsetWidth;
-            count = Math.trunc(this.container.offsetWidth / (elementWidth + this.gutter));
-            multiplier = (this.elements.length > count) ? count : this.elements.length;
-            containerWidth = multiplier * elementWidth + (multiplier - 1) * this.gutter;
-            leftSpace = (this.container.offsetWidth - containerWidth) / 2;
-            if (leftSpace > elementWidth) leftSpace = 0;
-            columns = new Array(count);
-
-            for(var i = 0; i < count; i++)
-                columns[i] = 0;
-
+            // Calculates positions
             for (
-                var i = 0,
-                    column = 0,
-                    containerHeight = 0,
-                    length = this.elements.length,
-                    node = this.elements.item(i);
+                var i = 0, j = 0, length = elements.length, node = elements.item(i), height = 0;
                 i < length;
-                ++i,
-                    column = (column + 1) % count,
-                    node = this.elements.item(i)
+                ++i, j = i % columnCount, node = elements.item(i)
             ) {
                 var left, top;
 
-                left = column * elementWidth + leftSpace;
-                if (column > 0)
-                    left += column * this.gutter;
-
-                top = columns[column] | 0;
+                left = offset + columnWidth * j;
+                top  = columnOffset[j];
 
                 node.style.position = "absolute";
                 node.style.left = left + "px";
                 node.style.top = top + "px";
 
-                columns[column] += node.offsetHeight + this.gutter;
+                columnOffset[j] += node.offsetHeight + gutter;
 
-                if (columns[column] > containerHeight)
-                    containerHeight = columns[column];
+                if (columnOffset[j] > height) height = columnOffset[j];
             }
 
-            containerHeight = containerHeight - this.gutter;
+            height = height - gutter;
+            container.style.position = "relative";
+            container.style.height = height + "px";
 
-            this.container.style.height = containerHeight + "px";
+        }
+
+        /*
+        * Updates events and elements
+        */
+        LayoutManager.prototype.update = function () {
+            this.elements = this.select();
+
+            Resizer.add(this.container, this.$rearrangePointer);
+            for (
+                var i = 0, length = this.elements.length, node = this.elements.item(i);
+                i < length;
+                ++i, node = this.elements.item(i)
+            ) {
+                Resizer.add(node, this.$rearrangePointer);
+            }
+        }
+
+        /*
+        * Rearranges all elements
+        */
+        LayoutManager.prototype.rearrange = function () {
+            if (this.$lastRAF) cancelFrame(this.$lastRAF);
+            this.update();
+            requestFrame(layout, null, [this.container, this.elements, this.gutter]);
         }
 
         return LayoutManager;
